16241358d6b71d738f53adda6e1f7ac68f488cb2
Sithi <sithivignesh11@gmail.com>
2026-06-16 23:07:59 +0530

    added resume generator - created a function to call the LLM to create the LaTeX code for the new resume - the input field contains fit_score, fit_summary, gaps and suggestions - so generate_resume endpoint will also call the analyzer function for those data - since to generate resume 2 LLM calls happens the time it takes is also soo high (nearly 3 mins) - i need to find a way to reduce the wait time.

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, UploadFile, File, Form
 from services.rag_service import resume_processor, embeding
 from services.websearch import research
 from db.vector_store import retrive
-from services.llm_service import analyzer
+from services.llm_service import analyzer, resume_generator
 import asyncio
 
 router  = APIRouter()
@@ -14,10 +14,26 @@ async def analyze(company_name: str = Form(...), job_description: str = Form(...
     tavily_context = output[1]
     query = embeding(job_description)
     retrived_data = retrive(query, 6)
+
     results = analyzer(company_name, job_description, tavily_context, retrived_data)
+
     return results
 
 @router.post("/generate-resume")
-def generate_resume(resume_file: UploadFile = File(...), company_name: str = Form(...), job_description: str = Form(...), fit_score: float = Form(...)):
-    ans = {"latex_code": "..."}
-    return ans
+async def generate_resume(resume_file: UploadFile = File(...), company_name: str = Form(...), job_description: str = Form(...)):
+    file_bytes = await resume_file.read()
+    output = await asyncio.gather(resume_processor(file_bytes, resume_file.filename), research(company_name))
+    tavily_context = output[1]
+    query = embeding(job_description)
+    retrived_data = retrive(query, 6)
+
+    results = analyzer(company_name, job_description, tavily_context, retrived_data)
+
+    fit_score = results["fit_score"]
+    fit_summary = results["fit_summary"]
+    gaps = results["gaps"]
+    suggestions = results["suggestions"]
+
+    latex = resume_generator(retrived_data, company_name, job_description, fit_summary, gaps, suggestions, fit_score)
+    print(latex)
+    return {"latex_code": latex}
